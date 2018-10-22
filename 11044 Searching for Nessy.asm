4147b74e4247e1b87ae6f2a68141bf0e42bd397a
DK Shanto <36234859+DK-Shanto@users.noreply.github.com>
2018-10-22 23:53:21 +0600

    Add files via upload

---
+++ 11044 Searching for Nessy.asm
@@ -0,0 +1,107 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+.MODEL SMALL
+.STACK 100H
+.DATA 
+.CODE
+
+MAIN PROC
+    CALL SCAN   ;INPUT TESTCASE
+    MOV CX, 0   ;CLEAR CX
+    MOV CL, DL
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H
+    MOV DL, 13
+    INT 21H
+    
+    TESTCASE:
+     PUSH CX
+     MOV AX,0
+     CALL SCAN ;CALL SCAN PROCEDURE
+     MOV AL,DL
+     PUSH AX
+     MOV BX,0
+     CALL SCAN
+     MOV BL,DL
+     PUSH BX
+     MOV BX,3
+     POP AX
+     DIV BX
+     MOV DX,AX
+     POP AX
+     DIV BX
+     MUL DX
+     MOV AX,DX
+     MOV DL,0DH  ;FOR CARRRIAGE
+     INT 21H
+     MOV DL,0AH  ;FOR NEW LINE
+     INT 21H
+     CALL PRINT
+     POP CX
+    LOOP TESTCASE
+    RET
+MAIN ENDP
+     
+     
+     
+        
+
+
+;a procedure that read an integer value
+;of one or more digit
+;input is terminated by both space and new line
+;the inputed integer will be present in dl
+SCAN PROC       
+    MOV DX, 0
+    INPUT:
+    MOV AH, 1
+    INT 21h  
+    CMP AL, ' '
+    JE END
+    CMP AL, 13
+    JE END
+    PUSH AX
+    MOV AL, 10
+    MUL DL
+    MOV DL, AL
+    POP AX
+    SUB AL, '0'
+    ADD DL, AL
+    JMP INPUT
+    END: 
+    RET
+SCAN ENDP 
+
+;a procedure that prints an integer
+;consisting of one or more digits
+;the integer must be present in ax
+PRINT PROC
+    XOR CX, CX
+    LOOP1:
+    CWD 
+    MOV BX, 10
+    IDIV BX
+    PUSH DX
+    INC CX
+    CMP AX, 0
+    JG LOOP1
+    
+    LOOP2:
+    POP DX 
+    ADD DX, '0'
+    MOV AH, 2
+    INT 21h
+    LOOP LOOP2  
+    RET
+PRINT ENDP  
+
+
+
+
+
+
